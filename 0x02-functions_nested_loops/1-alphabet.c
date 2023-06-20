7782873f83c5447420d0d62d3fcf8ab86fd15ade
KwemoiBruno <kwemoibruno7@gmail.com>
2023-06-21 02:19:32 +0300

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowecase followed by a new line
+ * print_alphabet - a function that prints the alphabet,
+ *in lowecase followed by a new line
  * Return: void
  */
 void print_alphabet(void)
