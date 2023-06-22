7871980866b019e17c65ee969df86142305e42d4
KwemoiBruno <kwemoibruno7@gmail.com>
2023-06-22 11:45:03 +0300

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * main - check if function gives correct output
+ * @i: the integer to be passed to the function
+ *
+ */
+
+int main(void)
+{
+  int i;
+  
+  i = 0;
+  positive_or_negative(i);
+
+  return (0);
+}
+
