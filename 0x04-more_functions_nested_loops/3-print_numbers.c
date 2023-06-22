d5447d30e2ce252358c75c949ab5ce7bc2484d1f
KwemoiBruno <kwemoibruno7@gmail.com>
2023-06-22 21:55:47 +0300

    first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: always zero
+ */
+void print_numbers(void)
+{
+int i = '0';
+while (i <= '9')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
